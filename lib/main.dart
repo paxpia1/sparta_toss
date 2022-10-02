6dbe47780eb20ab94ba80c489d45030f0b628523
paxpia1 <paxpia@gmail.com>
2022-10-02 23:53:54 +0900

    third commit

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:chang_mini/pages/splash_page.dart';
+import 'package:chang_mini/services/bank_service.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SharedPreferences prefs = await SharedPreferences.getInstance();
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => BankService(prefs)),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      home: SplashPage(),
+    );
+  }
+}
